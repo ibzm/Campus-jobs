ea5f1e06f4f600454c0c1c20671e6fce2da928bf
ibzm <ibzm99@outlook.com>
2025-02-26 18:08:50 +0000

    Fix laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,13 +16,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/{category}', function ($category) {
-    return view($category);
-})->name('{category}');
-
-// student paths 
-Route::get('/student', [StudentController::class, 'index'])
-->name('student');
 
 require __DIR__.'/auth.php';
